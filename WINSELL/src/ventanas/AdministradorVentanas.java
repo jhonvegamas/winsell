21343089f0abf5a9a0c517a3ab04ab47728fcb74
llJuan_Pabloll <>
2016-12-05 15:19:20 -0500

    Se elimina Jfacturas1.java, el proyecto queda funcional

--- WINSELL/src/ventanas/AdministradorVentanas.java
+++ WINSELL/src/ventanas/AdministradorVentanas.java
@@ -15,7 +15,6 @@ import javax.swing.JLayeredPane;
 public class AdministradorVentanas {
 
     private static JFacturas ftc;
-    private static JFacturas1 ftc1;
     private static RegistroCliente rc;    
     private static RegistroVendedor rv;
     private static RegistroArticulo ra;
@@ -36,23 +35,6 @@ public class AdministradorVentanas {
         }
         activarVentana(dp, ftc);
     }
-    public static void mostrarVentanaFactura2(JDesktopPane dp) {
-        if (ftc1 != null && !ftc1.isShowing()) {
-            ftc1.show();
-            dp.remove(ftc1);
-            try {
-                dp.add(ftc1, JLayeredPane.DEFAULT_LAYER);
-            } catch (IllegalArgumentException ex) {
-                dp.add(ftc1, JLayeredPane.DEFAULT_LAYER);
-            }
-        }
-        if (ftc1 == null) {
-            ftc1 = new JFacturas1();
-            dp.add(ftc1, JLayeredPane.DEFAULT_LAYER);
-        }
-        activarVentana(dp, ftc1);
-    }
-
     public static void mostrarVentanaVendedor(JDesktopPane dp) {
         if (rv != null && !rv.isShowing()) {
             rv.show();
